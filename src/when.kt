78f9fc0899628d137f2a70d6b2b65b7796b4c615
joshua.qa <jgchoi.qa@gmail.com>
2018-01-03 22:54:14 +0900

    class, enum 연습 (kotlin in action 2장)

--- src/when.kt
+++ src/when.kt
@@ -14,8 +14,33 @@ fun main(args: Array<String>) {
         isOdd(x) -> print("odd (function)")
         else -> print("even (function)")
     }
+
+    var point = Point(10, 20)
+
+    var a = point.x;
+
+    println(a)
+
+    // enum class (Java는 enum으로 쓰지만 kotlin은 enum class로 써야한다)
+    println(Color.BLUE.rgb())
 }
 
 fun isOdd(x: Int): Boolean {
     return x % 2 == 1
+}
+
+class Point(var x: Int, var y: Int)
+
+enum class Lang {
+    JAVA, CLANG, CPP, KOTLIN, RUST, SWIFT
+}
+
+enum class Color(
+    val r: Int, val g: Int, val b: Int
+) {
+    RED(255, 0, 0), ORANGE(255,165, 0),
+    YELLOW(255,255,0), GREEN(0, 255, 0), BLUE(0, 0, 255),
+    INDIGO(75, 0, 130), VIOLET(238, 130, 238);
+
+    fun rgb() = (r * 256 + g) * 256 + b
 }
